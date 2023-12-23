bed8163f6d8c45b19fabf535c42588f927eae88b
Jack Matusiewicz <jack.matusiewicz@outlook.com>
2023-12-23 21:08:28 +0000

    Moving client code to a source file

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main() {
     std::time_t time = std::chrono::system_clock::to_time_t(current_time);
     std::string message = std::string("The time is now: ") + std::ctime(&time);
     std::vector<char> data(message.begin(), message.end());
-    client.send_data(data);
+    auto _ = client.send_data(data);
     auto received_result = client.receive_data();
     if (!received_result.is_error()) {
         auto received_data = received_result.get_success();
